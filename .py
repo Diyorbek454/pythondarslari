996e22ebe565f9b7481a246fc2061fddc62d4393
kenjayev <dkenjayev134@gmail.com>
2025-11-01 17:36:03 +0500

    Create .py

---
+++ .py
@@ -0,0 +1,117 @@
+#1-masala
+#L = int(input("L = "))
+#L_metr = L // 100
+#print(L_metr)
+
+
+
+
+
+#2-masala
+#M = int(input("M = "))
+#M_tonna = M// 1000
+#print(M_tonna)
+
+
+
+
+
+
+#3-masala
+#K = int(input("K = "))
+#K_kb = K// 1024
+#print(K_kb)
+
+
+
+
+
+
+#4-masala
+#A = int(input("A = "))
+#B = int(input("B= "))
+#kesma = A // B 
+#print(kesma)
+
+
+
+
+#5-masala
+#A = int(input("A = "))
+#B = int(input("B= "))
+#kesma = A % B 
+#print(kesma)
+
+
+
+
+#6-masala
+#A = int(input("A = "))
+#B = int(input("B = "))
+#on = A//B
+#bir = A%B
+#print(on, bir)
+
+
+
+#7-masala
+#a = int(input("a ="))
+#b = int(input("b ="))
+#d = a + b
+#print(d)
+
+
+
+#8-masala
+#ab = int(input("a ="))
+#son = (ab % 10)*10 + ab //10
+#print(son)
+
+
+
+
+#9-masala
+#B = int(input("Uch honali son kiriting= "))
+#uch =  B//  100
+#print(uch)
+
+
+
+#10-masala
+#n = int(input("Uch xonali son kiriting: "))
+#n = abs(n)
+#unit = n % 10           
+#tens = (n // 10) % 10   
+#print("Birliklar:", unit)
+#print("Ongliklar:", tens)
+
+
+#11-masala
+#A = int(input("A = "))
+#B = int(input("B= "))
+#yig = A+B
+#print(yig)
+
+
+
+
+#12-masala
+#abc = int(input("uch xonali son kiriting"))
+#cba = (abc % 10) * 100 + ((abc // 10) %10) * 10 + abc // 100
+#print(cba)
+
+
+##13-masala
+#son = int(input("3xonali son kiriting = "))
+#natija = (son % 100) * 10 + son // 100
+#print(natija)
+
+
+#14-masala
+#abc = int(input("uch xonali son kiriting"))
+#cba = (abc % 10) * 100 + (abc // 10) 
+#print(cba)
+
+
+
+#15-masala
