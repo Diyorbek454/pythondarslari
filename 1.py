f9d681f5b59698c3a257cd5ccec6126b05574d9e
kenjayev <dkenjayev134@gmail.com>
2025-11-04 18:05:09 +0500

    Update 1.py

---
+++ 1.py
@@ -0,0 +1,77 @@
+## Davlatlar ro'yxatini tuzamiz
+#countries = ["O'zbekiston", "Qozog'iston", "Tojikiston", "Qirg'iziston", "Turkmaniston", "Rossiya", "Xitoy", "AQSH", "Yaponiya", "Fransiya"]
+#
+## 1. Ro'yxatni konsolga chiqarish
+#print("Asl ro'yxat:")
+#print(countries)
+#
+## 2. Ro'yxatning uzunligini chiqarish
+#print("\nRo'yxat uzunligi:", len(countries))
+#
+## 3. sorted() yordamida tartiblangan ro'yxat
+#print("\nAlifbo bo'yicha tartiblangan ro'yxat (sorted() yordamida):")
+#print(sorted(countries))
+#
+## 4. sorted() yordamida teskari tartibda
+#print("\nTeskari tartibda tartiblangan ro'yxat (sorted(reverse=True)):")
+#print(sorted(countries, reverse=True))
+#
+## 5. Asl ro'yxatni yana chiqarish (o'zgarmagan holatda)
+#print("\nAsl ro'yxat (o'zgarmagan):")
+#print(countries)
+#
+## 6. reverse() yordamida ro'yxatni teskari chiqarish
+#countries.reverse()
+#print("\nreverse() yordamida teskari chiqarilgan ro'yxat:")
+#print(countries)
+#
+## 7. sort() yordamida ro'yxatni alifbo bo'yicha tartiblash
+#countries.sort()
+#print("\nsort() yordamida alifbo bo'yicha tartiblangan ro'yxat:")
+#print(countries)
+#
+## 8. sort() yordamida teskari alifbo tartibida
+#countries.sort(reverse=True)
+#print("\nsort() yordamida teskari alifbo tartibida:")
+#print(countries)
+#
+# Juft sonlar ro'yxatini yaratamiz
+#even_numbers = list(range(120, 1201, 2))
+#n = len(even_numbers)
+#head = even_numbers[:20]
+#mid_start = max(0, n//2 - 10)
+#mid_end = min(n, n//2 + 10)
+#middle = even_numbers[mid_start:mid_end]
+#tail = even_numbers[-20:]
+#print("Boshi:", head)
+#print("O'rtasi:", middle)
+#print("Oxiri:", tail)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
