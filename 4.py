cb072e6952b5c06a46275e9da95c0b9aaf2ff6e8
kenjayev <dkenjayev134@gmail.com>
2025-11-27 17:06:48 +0500

    python
    python masalalar

---
+++ 4.py
@@ -0,0 +1,121 @@
+import math
+# 16 - masala
+#a = int(input("a = "))
+#b = int(input("b = "))
+#c = int(input("c = "))
+#if a < b < c :
+#    a = 2 * a
+#    b = 2 * b
+#    print(f"{a} = , {b} = , {c} = ")
+#else : 
+#    a = -a
+#    b = -b
+#    c = -c
+#    print(f"= {a} , {b} , {c}")    
+
+
+#17 - masala
+#a = int(input("a = "))
+#b = int(input("b = "))
+#c = int(input("c = "))
+#if  a < b < c :
+#    a = 2 * a
+#    b = 2 * b 
+#    c = 2 * c
+#elif a > b > c :
+#    a = 2 * a
+#    b = 2 * b 
+#    c = 2 * c
+#else : 
+#    a = -a
+#    b = -b
+#    c = -c
+#print(f"{a} , {b}, {c}")
+ 
+
+
+#18 - masala
+#a = int(input("a ="))
+#b = int(input("b ="))
+#c = int(input("c ="))
+#if a == b:
+#    print(3)
+#elif a == c :
+#    print(2)
+#else : 
+#    print(1)
+
+
+#19 - masala
+#a = int(input("a ="))
+#b = int(input("b ="))
+#c = int(input("c ="))
+#d = int(input("d ="))
+#if a == b :
+#    print(4)
+#elif a == c :
+#    print(3)
+#elif a == d :
+#    print(2)
+#else : 
+#    print(1)
+ 
+     
+
+#20 - masala
+#a = int(input("a ="))
+#b = int(input("b ="))
+#c = int(input("c ="))
+#masafoA_b = abs(a -b)
+#masafoA_c = abs(a - c)
+#if masafoA_b < masafoA_c:
+#    print(f"a nuqtaga eng yaqin nuqta b va ular orasidagi masofa {masafoA_b}")
+#else : 
+#    print(f"a nuqtaga eng yaqin c va ular orasidagi masoa{masafoA_c}")
+
+
+
+
+
+
+
+
+
+
+
+#24 - masala 
+x = int(input("X ni kiritng : " ))
+if x < 0 :
+#   f = 2 * math.sin(x)   
+# else :
+  f = x - 6
+  print(f"f(x) = {f}")
+
+
+
+
+
+#25-masala
+x = float(input("X ni kiriting"))
+if x < - 2 and x > 2 :
+  f = 2  * x
+  print("f(x) =", f)
+
+
+
+
+
+
+
+
+
+#26-masala 
+x = float(input("X ni kirting: "))
+if x <= 0: 
+ f = -x
+elif 0 < x < 2:
+  f = x **2
+else:
+  f = 4
+  print("f(x) =", f)
+  
