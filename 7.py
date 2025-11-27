ccab71ae00e2538db8421b7f6071c35904d77de0
kenjayev <dkenjayev134@gmail.com>
2025-11-27 17:07:39 +0500

    python
    python masalalar

---
+++ 7.py
@@ -0,0 +1,34 @@
+##1 - masala Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
+#ismlar = ["Abdullajon", "Javohir", "Bekmurot", "Isroil", "Muslima"]
+#for ism in ismlar:
+#    print(f"Salom, {ism}!")
+#
+#
+##2- masala Yuoqirdagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring (n o'rniga kod necha marta takrorlanganini yozing)
+#print(f"Kod {len(ismlar)} marta takrorlandi")
+#
+##3 - masala 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
+#for son in range(11, 100, 2): 
+#    print(son ** 3)           
+
+
+#4 -  masala Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang, va kinolar degan ro'yxatga saqlab oling. Natijani konsolga chiqaring.
+#kinolar = []  
+#
+#for i in range(5):
+#    kino = input(f"{i+1}-kinoni kiriting: ")
+#    kinolar.append(kino)
+#
+#print("Sizning sevimli kinolaringiz:", kinolar)
+
+
+#5 - masala Foydalanuvchidan bugun nechta odam bilan uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
+#odamlar = []
+#
+#soni = int(input("Bugun nechta odam bilan uchrashdingiz? "))
+#
+#for i in range(soni):
+#    odamlar.append(input("Ismini kiriting: "))
+#
+#print(odamlar)
+
