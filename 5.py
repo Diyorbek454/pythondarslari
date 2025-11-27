7d924b7772e027151462d3cc9b4c668d3b05a210
kenjayev <dkenjayev134@gmail.com>
2025-11-27 17:07:12 +0500

    python
    python masalalar

---
+++ 5.py
@@ -0,0 +1,64 @@
+#1 , 2 -masala
+# ismlar = ["isroil" , "abdullajon" , "shabnam" ]
+#print(f"{ismlar[0]} ,  hello , {ismlar[1]} , hello , {ismlar[2]}, h")
+
+
+#3 - masala  sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy, butun, o'nlik). 
+#sonlar = [12, -7, 3.14, 0, -2.5, 100, 8.9]
+#print(sonlar)
+
+
+#4 - masalaYuqoridagi ro'yxatdagi sonlar ustida turli arifmetik amallar bajarib ko'ring. Ro'yxatdagi ba'zi sonlarning qiymatini o'zgartiring, ba'zilarini esa almashtiring. 
+#sonlar = [12, -7, 3.14, 0, -2.5, 100, 8.9]
+#print(sonlar[0] + 10)
+
+#5 - masalat_shaxslarva z_shaxslar degan 2 ta ro'yxat yarating va biriga o'zingiz eng ko'p hurmat qilgan tarixiy shaxslarning, ikkinchisiga esa zamonamizdagi tirik bo'lgan shaxslarning ismini kiriting. 
+
+#t_shaxs = ["Alisher Navoiy" ,  "Amir Temur" , "Jaloliddin"]
+#z_shaxs = ["elon mask" , "bilgest" , "trump"]
+#print(t_shaxs)
+#print(z_shaxs)
+
+
+#6 - masala Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
+#t_shaxs = ["Alisher Navoiy", "Amir Temur", "Jaloliddin"]
+#z_shaxs = ["elon mask", "bilgest", "trump"]
+#tarixiy = t_shaxs.pop()
+#zamonaviy = z_shaxs.pop()
+#
+#print(f"Tarixiy shaxs: {tarixiy}, Zamonaviy shaxs: {zamonaviy}")
+
+
+
+#7 - masala friendsnomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga chaqirmoqchi bo'lgan do'stlaringizni kiriting. 
+#ism = []
+#ism.append("Ali")
+#ism.append("Vali")
+#ism.append("Gulbahor")
+#ism.append("Sardor")
+#ism.append("Nilufar")
+#ism.append("Jamshid")
+#print(ism)
+
+
+#8 - masala Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang. 
+#ism = ["Ali", "Vali", "Gulbahor", "Sardor", "Nilufar", "Jamshid"]
+#ism.remove("Sardor")
+#ism.remove("Nilufar")
+#print(ism)
+
+#9 - masala
+#ism = ["Ali", "Vali", "Gulbahor", "Jamshid"]
+#ism.append("Diyor")
+#ism.insert(0, "Malika")
+#ortasi = len(ism) // 2
+#ism.insert(ortasi, "Bekzod")
+#print(ism)
+
+
+#10 - masala
+#friends = ["Ali", "Vali", "Gulbahor", "Jamshid", "Diyor"]
+#mehmonlar = []
+#mehmonlar.append(friends.pop(1)) 
+#print("Friends ro'yxati:", friends)
+#print("Mehmonlar ro'yxati:", mehmonlar)
