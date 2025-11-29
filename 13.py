5ee391af734d4b34d677abd21aacf9bfbb22909b
kenjayev <dkenjayev134@gmail.com>
2025-11-29 04:52:36 -0800

    Create 13.py
    paython masala

---
+++ 13.py
@@ -0,0 +1,25 @@
+#k = int(input())
+#n = int(input())
+#for i in range(n):
+#  print(k)
+
+# 2 - masala
+#a = int(input('a = :'))
+#b = int(input('b = :'))
+#count = 0
+#for i in range(a, b+1) :
+#    print(i)
+#    count =  count + 1
+#    print('sikl', count, 'marta takrorlandi')
+
+
+# 3 - masala
+a = int(input('a = '))
+b = int(input('b = '))
+
+count = 0
+for i in range(b - 1, a, - 1):
+     print(i)
+
+count += 1
+print("Soni:", count)
