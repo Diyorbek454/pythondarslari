03cb8c2729b3e2db00c4ccf59962df5c02641fe2
kenjayev <dkenjayev134@gmail.com>
2025-12-06 04:20:18 -0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -24,18 +24,33 @@
 #    count = count + 1  
 #print(A)
 #
-#3 - masala
-while true :
-    yosh = int(input('Yoshingizni kiriting : '))
-    if yosh <= 7 :
-        narx = 2000
-    elif yosh <= 18:
-        narx = 3000
-    elif yosh <= 65:
-        narx = 10000
-    else :
-        narx = 0
-    print(f"sizga chipta narxi {narx} som")
-    javop = input("Yana davom etasizmi ? Dasturni tugatish uchun, 'exit' , yoki 'quit' ni yuborin" )
-    if (javop.lower() == 'quit' or javop.lower() == 'exit'):
-        break 
+#(3) - masala
+#while true :
+#    yosh = int(input('Yoshingizni kiriting : '))
+#    if yosh <= 7 :
+#        narx = 2000
+#    elif yosh <= 18:
+#        narx = 3000
+#    elif yosh <= 65:
+#        narx = 10000
+#    else :
+#        narx = 0
+#    print(f"sizga chipta narxi {narx} som")
+#    javop = input("Yana davom etasizmi ? Dasturni tugatish uchun, 'exit' , yoki 'quit' ni yuborin" )
+#    if (javop.lower() == 'quit' or javop.lower() == 'exit'):
+#        break 
+
+#3 - masala 
+#savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+#savol += "Musbat son kiriting "
+#savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+#
+#while True:
+#    qiymat = input(savol)
+#    if qiymat=='exit':
+#        break
+#    elif float(qiymat)<0:
+#        continue # agar foydalanuvchi manfiy son kiritsa tsiklni takrorlaymiz
+#    else:
+#        ildiz = float(qiymat)**(0.5)
+#        print(f"{qiymat} ning ildizi {ildiz} ga teng")
